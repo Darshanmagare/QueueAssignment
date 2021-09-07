7143730baede67121d8555b20f85ea05f91ea3d2
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:20:30 +0530

    Add project files.

---
+++ QueueAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace QueueAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueDemo demo = new QueueDemo();
+            demo.QueueOps();
+           
+        }
+    }
+
+    public class QueueDemo
+    {
+        public void QueueOps()
+        {
+            Queue q = new Queue();
+            Console.WriteLine("********Queue Operations*******");
+
+
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
+
+            q.Dequeue();
+
+            foreach (int items in q)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadKey();
+        }
+    }
+    
+    
+}
